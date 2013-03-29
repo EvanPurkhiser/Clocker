297673d278cad6cc97e6d87f45cfaa310c6e82cf
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-03-29 08:52:08 -0400

    Initalize the user from session on each request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
 
   layout 'layouts/base'
+  before_filter :load_user
+
+  protected
+
+  # Set the user attribute to the currently logged in user if they're currently
+  # logged in. If not set the user attribute to false
+  def load_user
+    @user = User.find(session[:user_id]) rescue false
+  end
+
 end
