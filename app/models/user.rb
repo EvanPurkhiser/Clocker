21eefcd70e09611422648ca4d092a35cf9b2d69b
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-03-29 10:41:47 -0400

    Allow password to be mass assigned on Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  attr_accessible :email
+  attr_accessible :email, :password
 
   validates_presence_of :email
   validates_uniqueness_of :email
