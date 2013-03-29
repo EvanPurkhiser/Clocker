7fdb17dbb6eddd75767255f59a864ffeb08c656b
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-03-29 07:51:52 -0400

    Add basic user model with password validation

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'mysql2'
 gem 'mustache'
 gem 'mustache-rails', require: 'mustache/railtie'
 
+gem 'bcrypt-ruby', '~> 3.0.0'
 
 # Gems used only for assets and not required
 # in production environments by default.
---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  attr_accessible :email
+
+  validates_presence_of :email
+  validates_uniqueness_of :email
+
+  # Authenticate a user by a passed email and password.
+  #
+  # Returns: self, or false if authentication fails
+  def self.authenticate(email, password)
+    find_by_email(email).try(:authenticate, password)
+  end
+
+end
