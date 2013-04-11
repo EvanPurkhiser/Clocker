62b8179fd04f151947351f32abbe199969cd6386
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-04-10 20:52:37 -0400

    Add methods to the project to clock in and out

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,7 @@
 class Entry < ActiveRecord::Base
+
+  # Indicates that the entry is currently pending completion
+  scope :pending, where(:end_time => nil)
+
   attr_accessible :description, :end_time, :start_time
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,34 @@ class Project < ActiveRecord::Base
   attr_accessible :name, :user_id
 
   validates :name, presence: true
+
+  # Check if the user is currently clocked in for this project.
+  #
+  # This will return true if there is atleast one entry for this project that
+  # doesn't have a end_time set on it
+  def clocked_in?
+    return entries.pending.count > 0
+  end
+
+  # Clock in as long as we aren't currently clocked in
+  def clock_in
+    raise InvalidProjectState if clocked_in?
+
+    entries << Entry.new(:start_time => Time.now)
+    self
+  end
+
+  # Clock out as long as we are currently clocked in
+  def clock_out(attributes = {})
+    raise InvalidProjectState unless clocked_in?
+
+    attributes = attributes.merge :end_time => Time.now
+    entries.pending.first.update_attributes(attributes)
+    self
+  end
+
+  # An invalid project state exception means that we tried to clock in when we
+  # were already clocked in, or clock out when we were already clocked out
+  class InvalidProjectState < Exception; end
+
 end
